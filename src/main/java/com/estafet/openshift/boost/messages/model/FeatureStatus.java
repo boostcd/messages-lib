f0467899e0221aa980974bcf822a0a5ebe1d7321
unknown <iryna.poplavska@estafet.com>
2020-02-28 10:21:01 +0200

    add values to enums field

--- src/main/java/com/estafet/openshift/boost/messages/model/FeatureStatus.java
+++ src/main/java/com/estafet/openshift/boost/messages/model/FeatureStatus.java
@@ -1,5 +1,22 @@
 package com.estafet.openshift.boost.messages.model;
 
 public enum FeatureStatus {
-	NOT_STARTED, IN_PROGRESS, DONE
+	NOT_STARTED("Not Started"),
+	IN_PROGRESS("In Progress"),
+	DONE("Done"),
+	BLOCKED("Blocked");
+
+	private String value;
+
+	FeatureStatus(String value){
+		this.value=value;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
 }
