23b280ffa486f937beab26e8f8abe117df3e2a7a
Dennis Williams <dennyjoewilliams@yahoo.com>
2020-04-08 21:46:40 +0100

    We need to add the message back into so that we can replay incomplete features.

--- src/main/java/com/estafet/openshift/boost/messages/features/FeatureMessageBuilder.java
+++ src/main/java/com/estafet/openshift/boost/messages/features/FeatureMessageBuilder.java
@@ -10,6 +10,12 @@ public class FeatureMessageBuilder {
 	private FeatureStatus status;
 	private String lastUpdated;
 	private String featureURL;
+	private String message;
+
+	public FeatureMessageBuilder setMessage(String message) {
+		this.message = message;
+		return this;
+	}
 
 	public FeatureMessageBuilder setFeatureURL(String featureURL) {
 		this.featureURL = featureURL;
@@ -58,6 +64,7 @@ public class FeatureMessageBuilder {
 				"status", 
 				"title", 
 				"repo", 
+				"message",
 				"featureId", 
 				"featureURL");
 		FeatureMessage message = new FeatureMessage();
@@ -67,6 +74,7 @@ public class FeatureMessageBuilder {
 		message.setStatus(status);
 		message.setTitle(title);
 		message.setRepo(repo);
+		message.setMessage(this.message);
 		message.setFeatureId(featureId);
 		message.setFeatureURL(featureURL);
 		return message;
