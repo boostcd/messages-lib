1fd694ae7f86b2a0ad21110e3c14bb25c351914e
Dennis Williams <dennyjoewilliams@yahoo.com>
2020-04-10 21:14:32 +0100

    unmatched messages are now unnecessary

--- src/main/java/com/estafet/openshift/boost/messages/features/FeatureMessage.java
+++ src/main/java/com/estafet/openshift/boost/messages/features/FeatureMessage.java
@@ -98,5 +98,97 @@ public class FeatureMessage {
 			throw new RuntimeException(e);
 		}
 	}
+	
+	public static class FeatureMessageBuilder {
 
+		private String commitId;
+		private String repo;
+		private String featureId;
+		private String title;
+		private String description;
+		private FeatureStatus status;
+		private String lastUpdated;
+		private String featureURL;
+
+		private FeatureMessageBuilder() {}
+		
+		public FeatureMessageBuilder setFeatureURL(String featureURL) {
+			this.featureURL = featureURL;
+			return this;
+		}
+
+		public FeatureMessageBuilder setCommitId(String commitId) {
+			this.commitId = commitId;
+			return this;
+		}
+
+		public FeatureMessageBuilder setRepo(String repo) {
+			this.repo = repo;
+			return this;
+		}
+
+		public FeatureMessageBuilder setFeatureId(String featureId) {
+			this.featureId = featureId;
+			return this;
+		}
+
+		public FeatureMessageBuilder setTitle(String title) {
+			this.title = title;
+			return this;
+		}
+
+		public FeatureMessageBuilder setDescription(String description) {
+			this.description = description;
+			return this;
+		}
+
+		public FeatureMessageBuilder setStatus(FeatureStatus status) {
+			this.status = status;
+			return this;
+		}
+
+		public FeatureMessageBuilder setLastUpdated(String lastUpdated) {
+			this.lastUpdated = lastUpdated;
+			return this;
+		}
+
+		public FeatureMessage build() {
+			nullCheck(
+					"commitId",
+					"lastUpdated",
+					"status", 
+					"title", 
+					"repo", 
+					"featureId", 
+					"featureURL");
+			FeatureMessage message = new FeatureMessage();
+			message.setCommitId(commitId);
+			message.setDescription(description);
+			message.setLastUpdated(lastUpdated);
+			message.setStatus(status);
+			message.setTitle(title);
+			message.setRepo(repo);
+			message.setFeatureId(featureId);
+			message.setFeatureURL(featureURL);
+			return message;
+		}
+
+		private void nullCheck(String... fields) {
+			for (String field : fields) {
+				nullCheck(field);
+			}
+		}
+
+		private void nullCheck(String field) {
+			try {
+				if (this.getClass().getDeclaredField(field).get(this) == null) {
+					throw new MissingFieldException(field + " cannot be null");
+				}
+			} catch (IllegalArgumentException | IllegalAccessException | NoSuchFieldException | SecurityException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+	}
+	
 }
